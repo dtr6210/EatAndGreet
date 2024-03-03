1ae7d4f055e507c99641a85f04c42047d5603da3
dtr6210 <rialsdean@gmail.com>
2024-03-03 11:42:47 -0600

    updated advertisements and about page

--- react/src/pages/MainFeedPage.jsx
+++ react/src/pages/MainFeedPage.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 import PostCard from "../components/PostCard";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper"; 
+import Typography from "@mui/material/Typography"; 
 import AdvertisingCard from "../components/AdvertisingCard";
+
 
 export default function MainFeedPage() {
   const [posts, setPosts] = useState([]);
@@ -12,13 +15,13 @@ export default function MainFeedPage() {
     axios
       .get("http://localhost:8080/api/posts")
       .then((response) => {
-        console.log("Posts fetch:", response.data.data); // checking the fetched post
+        console.log("Posts fetch:", response.data.data); // Checking the fetched post
         setPosts(response.data.data);
       })
       .catch((error) => console.error("Error fetching posts:", error));
   }, []);
 
-  console.log(posts) //log the posts array to check for _id values
+  console.log(posts); // Log the posts array to check for _id values
 
   return (
     <Grid container spacing={2} mt={5}>
@@ -44,15 +47,24 @@ export default function MainFeedPage() {
       <Grid item xs={12} lg={3}>
         {/* Advertisement section */}
         <Box
+          elevation={3}
           sx={{
-            backgroundColor: "background.paper",
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
             padding: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 10,
+            // backgroundColor: grey[50], 
           }}
         >
+          <Typography variant="h6" sx={{ fontWeight: "bold", marginBottom: 2 }}>
+            Advertisements:
+          </Typography>
+          <AdvertisingCard />
+          <AdvertisingCard />
+          <AdvertisingCard />
+          <AdvertisingCard />
+          <AdvertisingCard />
           <AdvertisingCard />
         </Box>
       </Grid>
